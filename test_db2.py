76d915eb901a43196f53aeee8e93d2667695bfe9
lukas-schniepper <lukas.schniepper@gmail.com>
2025-05-13 21:01:13 +0000

    update db2

--- test_db2.py
+++ test_db2.py
@@ -1,5 +1,5 @@
-from AlphaMachine_core.db import init_db
-init_db()
+#from AlphaMachine_core.db import init_db
+#init_db()
 
 #poetry run streamlit run app/streamlit_app.py \ --server.address=0.0.0.0 --server.port=8501
 
